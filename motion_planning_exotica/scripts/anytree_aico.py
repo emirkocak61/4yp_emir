9d6ec960c064cba6f2cb0f5131be71c07483d39b
emirkocak61 <emirkocak061@gmail.com>
2023-11-27 14:38:36 +0000

    Better visualizations

--- motion_planning_exotica/scripts/anytree_aico.py
+++ motion_planning_exotica/scripts/anytree_aico.py
@@ -7,13 +7,15 @@ import math
 
 
 def figure_eight(t):
-    return array([0.0, math.sin(t * 2.0 * math.pi * 0.5) * 0.1, math.sin(t * math.pi * 0.5) * 0.2, 0.0, 0.0, 0.0])
+    return array([0.7, math.sin(t * 2.0 * math.pi * 0.5) * 0.1, math.sin(t * math.pi * 0.5) * 0.2 + 0.5, 0.0, 0.0, 0.0])
 
 
 exo.Setup.init_ros()
 solver = exo.Setup.load_solver(
     '{motion_planning_exotica}/resources/configs/follow_traj.xml')
 problem = solver.get_problem()
+q = array([0.5,0.1,0.5,0,0,0])
+problem.start_state = q
 
 for t in range(0, problem.T):
     if t < problem.T/5:
