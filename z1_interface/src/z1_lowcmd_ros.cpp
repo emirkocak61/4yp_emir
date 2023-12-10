e7a54559697aa130d3c0ebdd28d49ecebd82d414
emirkocak61 <emirkocak061@gmail.com>
2023-12-10 21:58:02 +0000

    Pub/sub working smoothly

--- z1_interface/src/z1_lowcmd_ros.cpp
+++ z1_interface/src/z1_lowcmd_ros.cpp
@@ -11,91 +11,102 @@ using namespace UNITREE_ARM;
 class UnitreeInterface : public unitreeArm {
 public:
     ros::NodeHandle nh_; //Node handle 
-    ros::Subscriber sub;
-    std::string ctrl_mode; //Subscriber to receive pose goals
-    //std::vector<double> KP, KW; //Variables to store control gains
+    ros::Subscriber sub; //Subscriber to receive pose goals
+    std::string ctrl_mode = "lowcmd";
+    std::vector<double> KP, KW; //Variables to store control gains
+    
 
-    UnitreeInterface(std::string control_mode) : unitreeArm(true) {
+    UnitreeInterface(std::string control_mode,std::vector<double> KP = {20,30,30,20,15,10}, 
+    std::vector<double> KW ={2000,2000,2000,2000,2000,2000}) : unitreeArm(true) {
         
         ROS_INFO_STREAM("Z1 Interface Node is running, waiting for motion plans");
         this->ctrl_mode = control_mode; //Defines which control mode to use
+        this->KP = KP;
+        this->KW = KW;
+
         sub = nh_.subscribe("/motion_plan", 10, &UnitreeInterface::z1command, this); //set up the subscriber
         }
     
     ~UnitreeInterface() {}
     
+    void set_ctrl_gain() {
+        ROS_INFO_STREAM("Setting control gain");
+        lowcmd->setControlGain(KP,KW);
+        sendRecv();
+    }
+    
+    void go_to_start() {
+        Vec6 home_pose;
+        double duration = 1000;
+        home_pose << 0.0, 0.0, -0.005, -0.074, 0.0, 0.0; 
+        run(home_pose, duration);
+    }
+
+    
+
     //A function to set the control mode of the arm, not sure if the error handling is necessary
     //I only added for the sake of experience and learning
     //TO-DO:  add other control modes
-    void set_ctrl_mode(std::string ctrl_mode) {
-        try {
-            check_ctrl_mode(ctrl_mode);
-            setFsm(ArmFSMState::PASSIVE);
-            sendRecv();
-            setFsm(ArmFSMState::LOWCMD);
-            sendRecv();
-        } catch (const std::runtime_error &e)  {
-            std::cerr << "Caught an exception: " << e.what() << std::endl;
-            //Set the control mode to passive
-            setFsm(ArmFSMState::PASSIVE);
-            sendRecv();
-        }
-        
+    void set_ctrl_mode() {
+    // Update the RecvState structure
+    sendRecv();
+    // Check if the current state is LOWCMD
+    if (_ctrlComp->recvState.state != ArmFSMState::LOWCMD) { 
+        // Set to LOWCMD if it's not already
+        ROS_INFO_STREAM("Setting control mode to lowcmd");
+        sendRecvThread->start();
+        setFsm(ArmFSMState::PASSIVE);
+        setFsm(ArmFSMState::LOWCMD);
+        sendRecvThread->shutdown();
+        return;  
     }
-
-    bool check_ctrl_mode(std::string mode) {
-        if (mode == "lowcmd") {
-            return true;
-        }
-        else {
-            throw std::runtime_error("No such control mode, setting to passive");
-        }
-
+    else {
+        ROS_INFO_STREAM("Control mode already in lowcmd");
+        return;   
     }
+    
+}
 
-    ~UnitreeInterface() {}
-
+    //The callback function for the /motion_plan topic
     void z1command(const trajectory_msgs::JointTrajectoryPointConstPtr &msg) {
         ROS_INFO_STREAM("Received Motion Plan, executing...");
         std::cout << std::fixed << std::setprecision(3);
-        sendRecvThread->start();
-        labelRun("startFlat");
-        setFsm(ArmFSMState::PASSIVE);
-        setFsm(ArmFSMState::LOWCMD);
-        std::vector<double> KP, KW;
-        KP = _ctrlComp->lowcmd->kp;
-        KW = _ctrlComp->lowcmd->kd;
-        _ctrlComp->lowcmd->setControlGain(KP, KW);
-        sendRecvThread->shutdown();
-        Vec6 initQ = lowstate->getQ(); //Get current arm position
+        set_ctrl_mode();
         double duration = 1000;
         std::vector<double> Q = msg->positions;
-        Vec6 targetQ; 
-        targetQ << Q[0], Q[1], Q[2], Q[3], Q[4], Q[5];
+        Vec6 motion_goal; 
+        motion_goal << Q[0], Q[1], Q[2], Q[3], Q[4], Q[5];
+        run(motion_goal, duration);
+        //go_to_start();
+        
+    }
+
+    //Function that implements the torque calculation and communication with arm
+    void run(Vec6 targetQ, double duration) {
+        Vec6 initQ = lowstate->getQ();
         Timer timer(_ctrlComp->dt);
         for(int i(0); i<duration; i++){
             q = initQ * (1-i/duration) + targetQ * (i/duration);
             qd = (targetQ - initQ) / (duration * _ctrlComp->dt);
             tau = _ctrlComp->armModel->inverseDynamics(q, qd, Vec6::Zero(), Vec6::Zero());
-            gripperQ = -(i/duration);
+            gripperQ = -0.001;
             
             setArmCmd(q, qd, tau);
-            setGripperCmd(gripperQ, gripperW, gripperTau);
+            setGripperCmd(gripperQ, 0, 0);
             sendRecv();
             timer.sleep();
         }
-        sendRecvThread->start();
-        setFsm(ArmFSMState::JOINTCTRL);
-        labelRun("startFlat");
-        setFsm(ArmFSMState::PASSIVE);
-        sendRecvThread->shutdown();
     }
 
 }; 
 
 int main(int argc,char** argv){
     ros::init(argc,argv, "z1_interface_node");
-    UnitreeInterface arm("lowcmd");
+    std::vector<double> gain = {40,60,60,40,30,20};
+    UnitreeInterface arm("lowcmd",gain);
+    arm.set_ctrl_mode();
+    arm.set_ctrl_gain();
+    arm.go_to_start();
     ros::spin();
 }
 
