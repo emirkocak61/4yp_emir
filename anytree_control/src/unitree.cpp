eeb627b178120da00930c03d9b70088c32b5291b
emirkocak61 <emirkocak061@gmail.com>
2024-04-11 07:40:06 +0100

    Added sim vs hardware functionality

--- anytree_control/src/unitree.cpp
+++ anytree_control/src/unitree.cpp
@@ -2,9 +2,16 @@
 
 int main(int argc,char** argv) {
     ros::init(argc,argv,"unitree");
-    UnitreeRos arm_interface;
-    arm_interface.startPublishing();
-    ros::spin();
-    arm_interface.stopPublishing();
-    return 0;
+    ros::NodeHandle nh;
+    bool isSim;
+    if (nh.getParam("isSim", isSim)) {
+        UnitreeRos arm_interface(isSim);
+        arm_interface.startPublishing();
+        ros::spin();
+        arm_interface.stopPublishing();
+        return 0;
+    } else {
+        ROS_WARN("Please set the 'isSim' parameter before running this file");
+        return -1;
+    }   
 }
